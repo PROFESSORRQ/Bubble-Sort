77c5a3f701e76476582bb926a546aaf8b15d0982
ANSH GUPTA <anshgupta661@gmail.com>
2020-12-16 14:35:53 +0530

    Bubble Sort Algorithm

---
+++ Bubble Sort.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <hashtable.h>
+#include <math.h>
+#include <vector>
+#include <string.h>
+using namespace std;
+#define Nmax INT_MAX;
+#define Nmin INT_MIN;
+// void print(int arr[], int n)
+// {
+//     for (int i = 0; i < n; i++)
+//     {
+
+//     }
+// }
+
+int main()
+{
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    int n, i, j, k, p, l, count = 0, flag = 0;
+    cin >> n;
+    int arr[n];
+    int minNo = Nmax;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+      for(int j=0;j<n-i;j++){
+          if(arr[j]>arr[j+1]){
+              int temp=arr[j];
+              arr[j]=arr[j+1];
+              arr[j+1]=temp;
+          }
+      }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
